ed4afbc004dcf8bab428a9fd445118b56df242b1
dajanto <dajanto.1337@hotmail.de>
2021-05-12 19:57:27 +0200

    [TASK] Add basic form

---
+++ src/components/InputForm.vue
@@ -0,0 +1,15 @@
+<template>
+  <InputForm>
+    <div class="InputForm">
+      Frequency (mhz) 
+      <input v-model.number="frequency" type="number">
+      <br>
+      Length (cm)
+      <input v-model.number="length" type="number">
+      <br>
+      Position of coil
+      <input v-model.number="position" type="number">
+      <br>
+    </div>
+  </InputForm>
+</template>
