f170fa4c3494750ca575c74d2aeb5c038abf8aab
Nikolas Hagelstein <nikolas.hagelstein@gmail.com>
2021-05-13 14:26:29 +0200

    [TASK] change markup

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
 </template>
 
 <script>
+
 import InputForm from './components/InputForm.vue'
 
 export default {
--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -1,15 +1,20 @@
 <template>
-  <InputForm>
-    <div class="InputForm">
-      Frequency (mhz) 
-      <input v-model.number="frequency" type="number">
-      <br>
-      Length (cm)
-      <input v-model.number="length" type="number">
-      <br>
-      Position of coil
-      <input v-model.number="position" type="number">
-      <br>
-    </div>
-  </InputForm>
+        <div class="InputForm">
+            <b-form>
+                <b-form-group
+                        id="input-group-1"
+                        label="Email address:"
+                        label-for="input-1"
+                        description="We'll never share your email with anyone else."
+                >
+                    <b-form-input
+                            id="input-1"
+                            type="email"
+                            placeholder="Enter email"
+                            required
+                    ></b-form-input>
+                </b-form-group>
+
+            </b-form>
+        </div>
 </template>
