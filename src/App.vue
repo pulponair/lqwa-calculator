9fe507422507c81a1dbc46bbcf52b3c2c9d1d3a9
Nikolas Hagelstein <nikolas.hagelstein@gmail.com>
2021-05-13 15:13:19 +0200

    [FEATURE] Add form fields

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
+    <h1>Loaded Quarter Wave Antenna Calculator</h1>
     <InputForm></InputForm>
   </div>
 </template>
@@ -16,13 +17,3 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -1,20 +1,60 @@
 <template>
-        <div class="InputForm">
-            <b-form>
-                <b-form-group
-                        id="input-group-1"
-                        label="Email address:"
-                        label-for="input-1"
-                        description="We'll never share your email with anyone else."
-                >
-                    <b-form-input
-                            id="input-1"
-                            type="email"
-                            placeholder="Enter email"
-                            required
-                    ></b-form-input>
+    <div>
+        <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+            <b-form-group
+                    id="input-group-1"
+                    label="Frequency in Mhz:"
+                    label-for="input-1"
+            >
+                <b-form-input
+                        id="input-1"
+                        v-model="form.frequency"
+                        type="email"
+                        required
+                ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-2" label="Length in m:" label-for="input-2">
+                <b-form-input
+                        id="input-2"
+                        v-model="form.length"
+                        required
+                ></b-form-input>
+            </b-form-group>
+
+            <b-form-group id="input-group-3" label="Distance of Coil from base in m:" label-for="input-3">
+                <b-form-input
+                        id="input-3"
+                        v-model="form.distanceOfCoilFromBase"
+                        required
+                ></b-form-input>
                 </b-form-group>
 
-            </b-form>
-        </div>
+            <b-button type="submit" variant="primary">Calculate</b-button>
+        </b-form>
+        <b-card class="mt-3" header="Form Data Result">
+            <pre class="m-0">{{ form }}</pre>
+        </b-card>
+    </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                form: {
+                    frequency: 7,
+                    length: 3,
+                    distanceOfCoilFromBase: 0,
+                },
+                show: true
+            }
+        },
+        methods: {
+            onSubmit(event) {
+                event.preventDefault()
+                alert(JSON.stringify(this.form))
+            }
+        }
+    }
+</script>
